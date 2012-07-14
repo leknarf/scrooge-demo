d97a2a0071f973d4dbd2ad8f1cf4e8f38e75f20a
Andrew Frankel <andrew@leknarf.net>
2012-07-14 15:22:25 -0400

    started breaking out framework

--- worker/app.js
+++ worker/app.js
@@ -34,5 +34,5 @@ app.get('/', routes.index);
 
 app.listen(3000, function(){
   console.log("Scrooge worker listening on port %d in %s mode", app.address().port, app.settings.env);
-	reqHandler.getQueue();
+  reqHandler.getQueue();
 });
